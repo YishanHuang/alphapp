6c6e8a058eb96f5cd23309ae3b421409dca98214
YishanHuang <hyishan@hawk.iit.edu>
2015-03-03 19:25:09 -0600

    add edit and show function of the movie list" " git status q >

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,12 +9,30 @@ class EntriesController < ApplicationController
         redirect_to @entry
     end
     
-    private
-      def entry_params
-          params.require(:entry).permit(:title, :date)
-      end
+    def index
+        @entries = Entry.all
+    end
       
     def show
         @entry = Entry.find(params[:id])
+    end
+    
+    def edit
+        @entry = Entry.find(params[:id])
+    end
+    
+    def update
+        @entry = Entry.find(params[:id])
+        
+        if @entry.update(entry_params)
+            redirect_to @entry
+            else
+            render 'edit'
+        end
+    end
+    
+    private
+    def entry_params
+        params.require(:entry).permit(:title, :date)
     end
 end
